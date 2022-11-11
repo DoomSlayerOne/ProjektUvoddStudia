80bd3e86927cff08b6ebc975b484ba5c54e9b4ab
MartinKosec <m.kosec2503@gmail.com>
2022-11-11 15:13:03 +0100

    pridanie gitifnoru

---
+++ ProjektUvoddStudia/Pozadie.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Pozadie here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Pozadie {
+    /**
+     * Constructor for objects of class Pozadie
+     */
+    public Pozadie() {
+        // initialise instance variables
+    }
+}
